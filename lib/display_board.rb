f8176a983db275cf36d8c707bef3813ecee3831d
Christopher Williams <cwilliams.eats@gmail.com>
2020-11-16 22:37:32 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,6 +6,5 @@ def display_board(board)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
-  board = [" "," "," "," "," "," "," "," "," "]
-  
 end
+board = [" "," "," "," ","X"," "," "," "," "]
